59b85c238a34627559a96df8b29f113f7cb9fdb6
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-12 12:09:15 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -6,8 +6,8 @@ const UserProfile = () =>{
     const myContext = useContext(UserContext);
     return(
         <div>
-            <h1 id="name">{myContext.name}</h1>
-            <h1 id="age">{myContext.age}</h1>
+            <h1 id='name'>Name:- {myContext.name}</h1>
+            <h1 id='age'>Age:- {myContext.age}</h1>
         </div>
     )
 }
